4c6456123f15beb0ea44cf004c8e72075beb1cd8
SimonOmalley <91579437+SimonOmalley@users.noreply.github.com>
2022-09-17 12:02:51 +1000

    Working

--- windmill-dashboard-react-master/src/pages/Dashboard.js
+++ windmill-dashboard-react-master/src/pages/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-
 import InfoCard from '../components/Cards/InfoCard'
 import ChartCard from '../components/Chart/ChartCard'
 import { Doughnut, Line } from 'react-chartjs-2'
@@ -8,58 +7,36 @@ import PageTitle from '../components/Typography/PageTitle'
 import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from '../icons'
 import RoundIcon from '../components/RoundIcon'
 import response from '../utils/demo/tableData'
+import App from '../apiconn/fetchdata.js'
 import {
-  TableBody,
   TableContainer,
-  Table,
-  TableHeader,
-  TableCell,
-  TableRow,
   TableFooter,
-  Avatar,
-  Badge,
   Pagination,
 } from '@windmill/react-ui'
-
 import {
   doughnutOptions,
   lineOptions,
   doughnutLegends,
   lineLegends,
 } from '../utils/demo/chartsData'
-import { render } from 'react-dom'
-
 function Dashboard() {
   const [page, setPage] = useState(1)
-<<<<<<< Updated upstream
   const [data, setData] = useState([])
-=======
-  const [data, setData] = useState([])    
->>>>>>> Stashed changes
-
   // pagination setup
   const resultsPerPage = 10
   const totalResults = response.length
-
   // pagination change control
   function onPageChange(p) {
     setPage(p)
   }
-<<<<<<< Updated upstream
-
-=======
-  
->>>>>>> Stashed changes
   // on page change, load new sliced data
   // here you would make another server request for new data
   useEffect(() => {
     setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage))
   }, [page])
-
   return (
     <>
-      <PageTitle>Dashboard</PageTitle>      
-
+      <PageTitle>Dashboard</PageTitle>
       {/* <!-- Cards --> */}
       <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
         <InfoCard title="Total Entries" value="6389">
@@ -70,7 +47,6 @@ function Dashboard() {
             className="mr-4"
           />
         </InfoCard>
-        
         <InfoCard title="New Entries" value="376">
           <RoundIcon
             icon={CartIcon}
@@ -79,7 +55,6 @@ function Dashboard() {
             className="mr-4"
           />
         </InfoCard>
-
         <InfoCard title="Money Simon needs for this Project" value="$ 46,760.89">
           <RoundIcon
             icon={MoneyIcon}
@@ -87,81 +62,25 @@ function Dashboard() {
             bgColorClass="bg-green-100 dark:bg-green-500"
             className="mr-4"
           />
-        </InfoCard>        
-
+        </InfoCard>
         <InfoCard title="Weeks until Uni ends forever" value="5">
           <RoundIcon
             icon={ChatIcon}
             iconColorClass="text-teal-500 dark:text-teal-100"
             bgColorClass="bg-teal-100 dark:bg-teal-500"
             className="mr-4"
-<<<<<<< Updated upstream
           />
         </InfoCard>
       </div>
-
       <TableContainer>
-        <Table>
-          <TableHeader>
-            <tr>
-              <TableCell>ID</TableCell>
-              <TableCell>Unidentified Photo</TableCell>
-              <TableCell>User's Description</TableCell>
-              <TableCell>Device ID</TableCell>
-              <TableCell>Time Recorded</TableCell>
-              <TableCell>Approve/Deny</TableCell>
-            </tr>
-          </TableHeader>
-          <TableBody>
-            {data.map((user, i) => (
-              <TableRow key={i}>
-                <TableCell>
-                  <div className="flex items-center text-sm">
-                    <Avatar className="hidden mr-3 md:block" src={user.avatar} alt="User image" />
-                    <div>
-                      <p className="font-semibold">{user.name}</p>
-                      <p className="text-xs text-gray-600 dark:text-gray-400">{user.job}</p>
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <span className="text-sm">$ {user.amount}</span>
-                </TableCell>
-                <TableCell>
-                  <Badge type={user.status}>{user.status}</Badge>
-                </TableCell>
-                <TableCell>
-                  <span className="text-sm">{new Date(user.date).toLocaleDateString()}</span>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        <TableFooter>
-          <Pagination
-            totalResults={totalResults}
-            resultsPerPage={resultsPerPage}
-            label="Table navigation"
-            onChange={onPageChange}
-          />
-        </TableFooter>
-=======
-          />          
-        </InfoCard>
-      </div>
-      
-      <TableContainer>             
-        <App />                 
->>>>>>> Stashed changes
+        <App />
       </TableContainer>
-
       <PageTitle>Charts</PageTitle>
       <div className="grid gap-6 mb-8 md:grid-cols-2">
         <ChartCard title="Revenue">
           <Doughnut {...doughnutOptions} />
           <ChartLegend legends={doughnutLegends} />
         </ChartCard>
-
         <ChartCard title="Traffic">
           <Line {...lineOptions} />
           <ChartLegend legends={lineLegends} />
@@ -170,5 +89,4 @@ function Dashboard() {
     </>
   )
 }
-
 export default Dashboard
